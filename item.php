ca39a551190383af26b89819298b25b19c534f6e
j-Tinga <justintinga321@gmail.com>
2020-12-07 12:38:50 +0800

    commit

--- item.php
+++ item.php
@@ -183,6 +183,7 @@
         <label for="inputName">Brand Name:</label>
         <input type="text" name="brandName" id="inputName" class="form-control" placeholder="Input Brand Name"}>
         <input type="submit" value="Add Brand" name="newBrand" class="btn btn-primary">
+        ksdjajsd
         </form>
         </div>';
     }
